ff5ccb7b9b86edf54b693d05244f40e3eb6133f8
johnnyalexandersepulveda@gmail.com <johnnyalexandersepulveda@gmail.com>
2016-09-29 07:55:13 -0500

    añado includes, pedazos de html que se repiten en las vistas

---
+++ includes/cabecera.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<header>
+		<img id="logo" src="imagenes/logoNetlabel.png" width="228.166px" height="85.461px">
+				<ul class="redes">
+					<li>
+						<a href="https://www.twitter.com/"> 
+							<img class="logosRedes" src="imagenes/redesNetlabel/twitter.png"/>
+						</a>
+					</li>
+					<li>
+						<a href="https://www.facebook.com/"> 
+							<img class="logosRedes" src="imagenes/redesNetlabel/facebook.png"/>
+						</a>
+					</li>
+					<li>
+						<a href="https://www.gmail.com/"> 
+							<img class="logosRedes" src="imagenes/redesNetlabel/gmail.png"/>
+						</a>
+					</li>
+					<li>
+						<a href="https://www.instagram.com/"> 
+							<img class="logosRedes" src="imagenes/redesNetlabel/instagram.png"/>
+						</a>
+					</li>
+				</ul>
+				<nav>
+					<ul>
+						<li class="menu">
+							<a href="index.php">Netlabel</a>
+						</li>
+						<li class="menu">
+							<a href="musica.php">musica</a>
+						</li>
+						<li id="menu1">
+							<a href="artistas.php">Artistas</a>
+							<ul id="submenuArtistas">
+								<li class="listaArtistas"><a href=""> artista  </a></li>
+								<li class="listaArtistas"><a href=""> artista  </a></li>
+								<li class="listaArtistas"><a href=""> artista  </a></li>
+								<li class="listaArtistas"><a href=""> artista  </a></li>
+								<li class="listaArtistas"><a href=""> artista  </a></li>
+								<li class="listaArtistas"><a href=""> artista  </a></li>
+								<li class="listaArtistas"><a href=""> artista  </a></li>
+							</ul>
+						</li>
+						<li class="menu">
+							<a href="contacto.php">Contacto</a>
+						</li>
+					</ul>
+				</nav>
+				<div id="triangulo"></div>
+			</header>
+</body>
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<footer>
+		<article class="seccionesFooter">
+			<h4> ZUTURA </h4>
+			<h5> 8bit - Circuit Bending NET LABEL </h5>
+		</article>
+		<article class="seccionesFooter">
+			<h6> Medellin Colombia 2016 </h6>
+		</article>
+		<article class="seccionesFooter">
+			<ol>
+				<li id="registrarse">
+					<a href="administrarUsuarios/registrarse.php"> registrarse </a>
+				</li>
+				<li id="entrar">
+					<a href="administrarUsuarios/entrar.php"> Entrar </a>
+				</li>
+			</ol>
+		</article>
+	</footer>
+</body>
+</html>
