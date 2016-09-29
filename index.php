060751bd57d15bb816013b196cfc2e26fc37af21
johnnyalexandersepulveda@gmail.com <johnnyalexandersepulveda@gmail.com>
2016-09-29 07:46:46 -0500

    añado vista index

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<title> Informacion </title>
+		<link rel="shortcut icon" href="imagenes/favicon.png">
+		<script language="javascript" type="text/javascript" src="libraries/jquery-2.2.1.min.js"></script>
+		<script language="javascript" type="text/javascript" src="js/main.js"></script>
+		<link href="estilos/estilosIndex.css" type="text/css" rel="stylesheet">
+	</head>
+	<body>
+		<div id="container">
+			<!-- CABECERA -->
+			<?php include 'includes/cabecera.php'; ?>
+			<!-- FIN CABECERA -->
+			<!-- CONTENIDO -->
+			<section>
+				<h1> ZUTURA </h1>
+				<p> Zutura es un Net Label independiente ubicado en Medellin(Colombia), trabajamos desde 2010. Nuestro trabajo abarca descargas y contenido relacionado con música 8bit, Circuit Bending y la cultura electronica DIY, todos nuestros trabajos bajos licencias creative commons y descargas gratuitas. 
+				<br></br>
+				La música producida mediante corto circuitos y 8bit nos son géneros musicales como tal, sin embargo; pueden contenedores de nuevos y pasados estilos, que tienen el valor de hacer uso de la tecnologia vieja y obsoleta como herramienta en comun. Nuestra familia de zuturados crece todos los dias.
+				<br></br>
+				Zutura inicialmente es creado por: BLIKA, PROGRAMMERCHIP. 
+				</p>
+				<div id="separador1">
+				<h2> envianos tu demo </h2>
+				<p> En Zutura estamos abiertos y dispuestos a recibir y publicar su material. Nos puede enviar su demo en zutura@gmail.com.
+				<br></br>
+				Con el fin de hacer una escucha vista previa rápida, usted debe enviar un enlace privado a su materia (La página web, Soundcloud, Vimeo, etc) o un enlace de descarga.  
+				<br></br>
+				 Su material debe ser inédito y siempre relacionado con el mundo del sonido de 8 bits y / o circuit bending. (Vamos a rechazar fakebit, DJ Sessions, cubiertas, material robado, etc). Las canciones pueden tener un número ilimitado de instrumentación, dispositivos, voces o post-procesamiento, pero la música que suena debe mantener una alta tecnología de 8 bits.
+				<br></br>
+				Una vez que tengamos la demo, vamos a pasar algún tiempo analizándolo y vamos a tratar de volver a usted tan pronto como sea posible. Si todo es fresco, te pediremos la siguiente información para poder liberarlo, a través de nuestra página web bajo una licencia Creative Commons (BY-NC-ND 3.0) Licencia.
+				</p>
+				<div id="separador2"></div>
+				<div id="muro"></div>
+				<div id="personaje1"></div>
+				<div id="personaje2"></div>
+			</section>
+			<!-- FIN CONTENIDO -->
+			<!-- footer -->
+			<?php include 'includes/footer.php'; ?>
+		</div>
+	</body>
+</html>
